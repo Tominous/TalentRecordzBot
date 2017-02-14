860dfc58167e193971734c37e3e68a9179837369
ChisdealHD <Chisdeal2014@hotmail.com>
2017-02-14 08:53:35 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1136,7 +1136,7 @@ app.get('/date', function(req, res){ res.send("Talntrecordz Date is "+ started +
 app.get('/uptime', function(req, res){ res.send("Talntrecordz Has Been Up For "+ started +""); });
 app.get('/specs', function(req, res){ res.send("xl\nSystem info: " + process.platform + "-" + process.arch + " with " + process.release.name + " version " + process.version.slice(1) + "\nProcess info: PID " + process.pid + " at " + process.cwd() + "\nProcess memory usage: " + Math.ceil(process.memoryUsage().heapTotal / 1000000) + " MB\nSystem memory usage: " + Math.ceil((os.totalmem() - os.freemem()) / 1000000) + " of " + Math.ceil(os.totalmem() / 1000000) + " MB\nBot info: ID " + bot.user.id + " #" + bot.user.discriminator + "\n") });
 
-app.listen(process.env.PORT || ${serverport});
+app.listen(process.env.PORT || + serverport);
 // END Roboto SETUP
 
 process.on("unhandledRejection", err => {
