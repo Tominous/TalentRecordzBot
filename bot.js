b13f0d4d0b29f758c3a39ce1c6496cc0a1455c45
ChisdealHD <Chisdeal2014@hotmail.com>
2017-02-23 10:14:00 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -1162,6 +1162,7 @@ bot.on('ready', function() {
             if (err) {
                 message.channel.sendMessage("ERROR has be MADE!" + err);
             }
+       });
     });
 }, 120000)
 });
