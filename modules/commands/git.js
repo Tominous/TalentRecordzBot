ecd11db359f51bfc192e6ea87b7823db0e9f35d7
ChisdealHD <Chisdeal2014@hotmail.com>
2017-10-06 09:30:08 +0100

    Update git.js

--- modules/commands/git.js
+++ modules/commands/git.js
@@ -3,8 +3,8 @@ const config = require('../../config.json');
 const prefix = config.prefix;
 
 module.exports = (bot) => {
-	bot.addTraditionalCommand("git", message => {
-		message.delete(1000)
+	bot.addCommand("git", (payload) => {
+	var message = payload.message
         message.channel.send("This bot Created by ChisdealHD at https://github.com/ChisdealHD/TalentRecordzBot/")
     })
 }
