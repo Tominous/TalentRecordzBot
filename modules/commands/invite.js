ed5cfc4d75e868a9dabc3c97b00e13d77b00601a
ChisdealHD <Chisdeal2014@hotmail.com>
2017-10-06 09:31:48 +0100

    Update invite.js

--- modules/commands/invite.js
+++ modules/commands/invite.js
@@ -3,8 +3,8 @@ const config = require('../../config.json');
 const prefix = config.prefix;
 
 module.exports = (bot) => {
-	bot.addTraditionalCommand("invite", message => {
-		message.delete(1000)
+	bot.addCommand("invite", (payload) => {
+	var message = payload.message
         message.channel.send("My OAuth URL: " + `https://discordapp.com/oauth2/authorize?permissions=1341643849&scope=bot&client_id=${config.client_id}`)
     })
 }
