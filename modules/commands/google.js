024ca02eb4ce8456587147f5210c9f2a2114bd03
ChisdealHD <Chisdeal2014@hotmail.com>
2017-10-06 09:30:38 +0100

    Update google.js

--- modules/commands/google.js
+++ modules/commands/google.js
@@ -3,8 +3,8 @@ const config = require('../../config.json');
 const prefix = config.prefix;
 
 module.exports = (bot) => {
-	bot.addTraditionalCommand("google ", message => {
-		message.delete(1000)
+	bot.addCommand("google ", (payload) => {
+	var message = payload.message
         var searchQuery = encodeURI(message.content.substring(8))
         var url = "https://www.google.com/search?q=" + searchQuery;
         message.channel.sendMessage(url + "\n Here Is Your Search!");
